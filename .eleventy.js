abd3e77aac5fe6cfe10f15d92872c235ceb3513f
Marco Colombo <mcolom4@uillinois.edu>
2024-06-28 16:06:46 -0500

    Added past events support

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,16 @@ module.exports = function (eleventyConfig) {
     });
   });
 
+  eleventyConfig.addFilter('pastEvents', function(events) {
+    const currentDate = new Date();
+    const currentDateString = Number(currentDate.toISOString().slice(0,10).replace(/-/g, ''));
+    return events.filter(event => {
+      const filename = event.filePathStem.split('/').pop();
+      const eventDate = Number(filename.slice(0,8));
+      return eventDate < currentDateString;
+    });
+  });
+
   return {
     pathPrefix: "/pose/", // The base path in github
     dir: {
