4fad09fc0610fec38ca6c77d3eaf8f41a0730640
Marco Colombo <mcolom4@uillinois.edu>
2024-10-09 10:40:09 -0500

    Add train material and update images

--- .eleventy.js
+++ .eleventy.js
@@ -20,10 +20,23 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('pastEvents', function(events) {
     const currentDate = new Date();
     const currentDateString = Number(currentDate.toISOString().slice(0,10).replace(/-/g, ''));
-    return events.filter(event => {
+    // return events.filter(event => {
+    //   const filename = event.filePathStem.split('/').pop();
+    //   const eventDate = Number(filename.slice(0,8));
+    //   return eventDate < currentDateString;
+    // });
+    // Filter past events
+    const pastEvents = events.filter(event => {
       const filename = event.filePathStem.split('/').pop();
-      const eventDate = Number(filename.slice(0,8));
+      const eventDate = Number(filename.slice(0,8)); // Extract event date from the filename
       return eventDate < currentDateString;
+    });
+
+    // Sort the past events in reverse order (most recent first)
+    return pastEvents.sort((a, b) => {
+      const aDate = Number(a.filePathStem.split('/').pop().slice(0,8));
+      const bDate = Number(b.filePathStem.split('/').pop().slice(0,8));
+      return bDate - aDate; // Reverse order sorting (descending)
     });
   });
 
